1eacf232549bcf85b9a9f75fd993b60d9836db4f
yogendramaurya55 <yogendramaurya9083@gmail.com>
2026-03-14 00:01:40 +0530

    problem statement 1.2

--- Spring-Training/Spring_Assignment_DI_13_03_2026/src/main/java/com/cg/bean/Employee.java
+++ Spring-Training/Spring_Assignment_DI_13_03_2026/src/main/java/com/cg/bean/Employee.java
@@ -4,107 +4,78 @@ public class Employee {
 	private int employeeId;
 	private String employeeName;
 	private double salary;
-	private String businessUnit;
+	private SBU businessUnit;
 	private int age;
-	
-	
-	
-	
-	
+
 	public Employee() {
 		super();
 	}
-
-
-
-
 
 	public int getEmployeeId() {
 		return employeeId;
 	}
 
-
-
-
-
 	public void setEmployeeId(int employeeId) {
 		this.employeeId = employeeId;
 	}
-
-
-
-
 
 	public String getEmployeeName() {
 		return employeeName;
 	}
 
-
-
-
-
 	public void setEmployeeName(String employeeName) {
 		this.employeeName = employeeName;
 	}
-
-
-
-
 
 	public double getSalary() {
 		return salary;
 	}
 
-
-
-
-
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
 
-
-
-
-
-	public String getBusinessUnit() {
+	public SBU getBusinessUnit() {
 		return businessUnit;
 	}
 
-
-
-
-
-	public void setBusinessUnit(String businessUnit) {
+	public void setBusinessUnit(SBU businessUnit) {
 		this.businessUnit = businessUnit;
 	}
-
-
-
-
 
 	public int getAge() {
 		return age;
 	}
 
-
-
-
-
 	public void setAge(int age) {
 		this.age = age;
 	}
 
-
-
-
+//	public void displayEmployeeDeatils() {
+//		System.out.println("Employee ID : " + employeeId);
+//		System.out.println("Employee Name : " + employeeName);
+//		System.out.println("Employee Salary : " + salary);
+//		System.out.println("Employee BU : " + businessUnit);
+//		System.out.println("Employee Age : " + age);
+//
+//	}
+	
+	
 
 	public void displayEmployeeDeatils() {
-		System.out.println("Employee ID : " + employeeId);
-		System.out.println("Employee Name : " + employeeName);
-		System.out.println("Employee Salary : " + salary);
-		System.out.println("Employee BU : " + businessUnit);
-		System.out.println("Employee Age : " + age);
+		
+		System.out.println("Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", salary=" + salary + ", age="
+				+ age + "]");
+		
+		getSbuDetails();
+		
+		
+
+	}
+
+	public void getSbuDetails() {
+		System.out.println("sbu details = SBU [ subCode =" + businessUnit.getSbuId() + "sbuName = " + businessUnit.getSbuName() + "sbuHead = " + businessUnit.getSbuHead() + " ]");
 		
 	}
+
 }
