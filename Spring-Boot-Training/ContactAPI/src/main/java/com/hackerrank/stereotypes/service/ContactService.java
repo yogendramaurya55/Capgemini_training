264f563521491dbb5c7296633a4a65a58cc139f5
yogendramaurya55 <yogendramaurya9083@gmail.com>
2026-03-19 19:10:39 +0530

    contactAPI fixed

--- Spring-Boot-Training/ContactAPI/src/main/java/com/hackerrank/stereotypes/service/ContactService.java
+++ Spring-Boot-Training/ContactAPI/src/main/java/com/hackerrank/stereotypes/service/ContactService.java
@@ -8,6 +8,10 @@ import com.hackerrank.stereotypes.repository.ContactRepository;
 @Service
 public class ContactService {
     ContactRepository contactRepository;
+    
+    public ContactService(ContactRepository contactRepository) {
+    	this.contactRepository = contactRepository;
+    }
 
     public Person retrieve(Integer id){
         return contactRepository.findById(id).get();
